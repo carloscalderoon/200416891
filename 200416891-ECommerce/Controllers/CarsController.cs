4fcdb3d6ecfbe4433673d08a056045dfbcea7adf
Carlos Calderon <calderoncarloos@gmail.com>
2019-05-27 18:03:37 -0400

    1Controller & 2Views

---
+++ 200416891-ECommerce/Controllers/CarsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _200416891_ECommerce.Models;
+
+namespace _200416891_ECommerce.Controllers
+{
+    public class CarsController : Controller
+    {
+        List<Car> carsList = new List<Car>()
+    {
+        new Car {Id = 1, Brand = "Tesla", Model = "Model 3", Type ="Electric", Year = 2019},
+        new Car {Id = 2, Brand = "Tesla", Model = "Model S", Type ="Electric", Year = 2019},
+        new Car {Id = 3, Brand = "Tesla", Model = "Model X", Type ="Electric", Year = 2019}
+    };
+
+    // GET: Cars - List of all Cars
+    public ActionResult Index()
+        {
+            // return Content("/Cars - This is a List of Cars");
+            return View(carsList);
+        }
+
+    //Details
+    public ActionResult Details(int? id)
+    {
+            if (id == null || id > carsList.Count)
+            {
+                return Content("Invalid Car Id");
+            }
+            var ind = Convert.ToInt32(id) - 1;
+            var car = carsList[ind];
+            return View(car);
+    }
+    }
+}
---
+++ 200416891-ECommerce/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace _200416891_ECommerce.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public String Brand { get; set; }
+        public String Model { get; set; }
+        public String Type { get; set; }
+        public int Year { get; set; }
+
+    }
+}
