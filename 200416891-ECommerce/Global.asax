520b7187cd7da6051b5176de81743fcbaffdc431
Carlos Calderon <calderoncarloos@gmail.com>
2019-05-27 15:29:08 -0400

    first commit

---
+++ 200416891-ECommerce/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_200416891_ECommerce.MvcApplication" Language="C#" %>
---
+++ 200416891-ECommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200416891_ECommerce.Startup))]
+namespace _200416891_ECommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
